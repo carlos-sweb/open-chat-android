9a480f306aab4011c33600757b51e3574dd73b74
carlos-sweb <c4rl0sill3sc4@gmail.com>
2019-11-03 17:22:42 -0300

    upload contry code

---
+++ code-contry/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+
+
+
+fs.readFile('index.html', {encoding:'utf8'},
+function(err, data){	
+if (err) throw err;
+var json = [];
+const $ = cheerio.load(data);
+
+$("table")
+.first()
+.find("tr").each(function(){
+	 
+	 let td = $(this).children("td");
+	 if(td.eq(0).text() !=""){
+	 var code = {
+	 	 contry:td.eq(0).text(),
+	 	 code:td.eq(1).text(),
+	 	 iso:td.eq(2).text()
+	 };
+	 json.push(code);
+	 }
+	 
+});
+///////
+fs.writeFile("dist/contrycode.js","var contrycode="+JSON.stringify(json)+";", function(err) { if(err) { return console.log(err); } console.log("The file was saved!"); }); 
+
+ });
+ 
+ 
