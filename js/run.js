b284984153f9d3e3b151f78c0cbd6baffea55680
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-03-05 10:42:24 -0300

    remove files

--- js/run.js
+++ js/run.js
@@ -1,8 +1,8 @@
-var css = ["node_modules/normalize.css/normalize.min.css",
-			"node_modules/flexboxgrid/dist/flexboxgrid.min.css",
-			"node_modules/spectre.css/dist/spectre.min.css",
-			"node_modules/spectre.css/dist/spectre-exp.min.css",
-			"node_modules/spectre.css/dist/spectre-icons.min.css"];			
+var css = ["normalize.min.css",
+			"flexboxgrid.min.css",
+			"spectre.min.css",
+			"spectre-exp.min.css",
+			"spectre-icons.min.css"];			
 css.forEach((item)=>{
 	
 	var stylesheet = window.loadCSS( item );
